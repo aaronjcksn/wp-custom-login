d5634530916cad1c2aaaa20b3e1dbece46580692
Aaron Jackson <aaron.jackson@red8interactive.com>
2015-07-22 18:15:56 -0500

    error messages now display on login form

--- templates/login_form.php
+++ templates/login_form.php
@@ -3,6 +3,15 @@
         <h2><?php _e( 'Sign In', 'praxis-login'); ?></h2>
     <?php endif; ?>
 
+        <!-- Show errors if there are any -->
+    <?php if ( count( $attributes['errors'] ) > 0 ) : ?>
+        <?php foreach ( $attributes['errors'] as $error ) : ?>
+            <p class="login-error">
+                <?php echo $error; ?>
+            </p>
+        <?php endforeach; ?>
+    <?php endif; ?>
+
     <form method="post" action="<?php echo wp_login_url(); ?>">
         <p class="login-username">
             <label for="user_login"><?php _e( 'Username', 'praxis-login' ); ?></label>
